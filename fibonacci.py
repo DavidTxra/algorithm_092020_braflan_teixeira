b8f161a39898aec1b607999335eb8dad982e2d84
DavidTxra <david.teixeira@edu.devinci.fr>
2020-10-01 10:00:48 +0200

    feat: find max

--- fibonacci.py
+++ fibonacci.py
@@ -10,6 +10,7 @@ class FibonacciHeap:
     def __init__(self):
         self.trees = []
         self.least = None
+        self.head = None
         self.count = 0
 
     # Ajoute une valeur dans l'arbre
@@ -17,15 +18,22 @@ class FibonacciHeap:
         new_tree = FibonacciTree(value)
         self.trees.append(new_tree)
         if (self.least is None or value < self.least.value):
-            self.least = new_tree
+            self.least = new_tree 
+        if (self.head is None or value > self.head.value):
+            self.head = new_tree        
         self.count = self.count + 1
 
     # Retourne la valeur minimum dans l'arbre
     def find_min(self):
         if self.least is None:
             return None
-        return self.least.value    
+        return self.least.value 
 
+    # Retourne la valeur maximum dans l'arbre
+    def find_max(self):
+        if self.head is None:
+            return None
+        return self.head.value    
 
 
 fibonacci_heap = FibonacciHeap()
@@ -34,9 +42,10 @@ fibonacci_heap.insert(7)
 fibonacci_heap.insert(3)
 fibonacci_heap.insert(17)
 fibonacci_heap.insert(24)
-fibonacci_heap.insert(1)
 fibonacci_heap.insert(12)
 fibonacci_heap.insert(3)
 fibonacci_heap.insert(4)
 
 print('Valeur minimum: ' + format(fibonacci_heap.find_min()))
+
+print('Valeur maximum: ' + format(fibonacci_heap.find_max()))
